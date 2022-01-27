91b6fd55daa5ade6123e2ea5019a354b07918e75
TMA\lxhong <lexuanhong913@gmail.com>
2022-01-27 16:18:58 +0700

    config angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './layout/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {  MessageService } from "primeng/api";
+import { ToastModule } from 'primeng/toast';
+import { MessagesModule } from 'primeng/messages';
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren:() => import('./layout/user/module/user.module').then(m=>m.UserModule)
+  },
+  {
+    path: 'user',
+    loadChildren:() => import('./layout/user/module/user.module').then(m=>m.UserModule)
+  },
+
+  {
+    path: 'admin',
+    loadChildren: ()=> import('./layout/admin/module/admin.module').then(m=>m.AdminModule)
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastModule,
+    MessagesModule
+    
+   
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
