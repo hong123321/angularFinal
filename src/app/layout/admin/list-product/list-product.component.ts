4ba2b9f990f51f955467b20178b408a341b5335b
TMA\lxhong <lexuanhong913@gmail.com>
2022-01-27 16:17:08 +0700

    create list product

---
+++ src/app/layout/admin/list-product/list-product.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../service/admin.service';
+@Component({
+  selector: 'app-list-product',
+  templateUrl: './list-product.component.html',
+  styleUrls: ['./list-product.component.css']
+})
+export class ListProductComponent implements OnInit {
+  products:any[];
+  vote:any
+  constructor(private data:AdminService) { }
+
+  ngOnInit(): void {
+    this.data.getProduct().subscribe(data=>{
+      this.products=data;
+      data.map(element => {
+        this.vote =  (element.vote) / 20
+      }
+      )
+    })
+  }
+
+}
