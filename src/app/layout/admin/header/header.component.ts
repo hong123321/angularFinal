dad177a974e6a0f20324d2ee7b5e7fa04ff69e6e
TMA\lxhong <lexuanhong913@gmail.com>
2022-01-27 16:17:50 +0700

    create Manubar for Admin Layout

---
+++ src/app/layout/admin/header/header.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  items: MenuItem[];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+      label: 'List User',
+      icon:'pi pi-users',
+      routerLink:'home'
+      },
+      {
+        label: 'List Product',
+        icon:'pi pi-shopping-cart',
+        routerLink:'listProduct'
+        },
+        {
+          label: 'Order Management',
+          icon:'pi pi-fw pi-file',
+          routerLink:'managementProduct'
+          } ,
+          {
+            label: 'List News',
+            icon:'pi pi-fw pi-file',
+            routerLink:'news'
+            }           
+    ]
+  }
+
+}
