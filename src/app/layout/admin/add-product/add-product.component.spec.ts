b0c29e74d6ee209a29b41848be993e3addb33ba6
TMA\lxhong <lexuanhong913@gmail.com>
2022-02-21 14:29:49 +0700

    FORMAT CODE

--- src/app/layout/admin/add-product/add-product.component.spec.ts
+++ src/app/layout/admin/add-product/add-product.component.spec.ts
@@ -1,16 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AddProductComponent } from './add-product.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Confirmation, ConfirmationService, MessageService } from 'primeng/api';
+import { AdminService } from '../../service/admin.service';
+import { of } from 'rxjs';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
-describe('AddProductComponent', () => {
+fdescribe('AddProductComponent', () => {
   let component: AddProductComponent;
   let fixture: ComponentFixture<AddProductComponent>;
+  const serviceAdmin = {
+    addProduct: jasmine.createSpy(),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddProductComponent ]
+      declarations: [AddProductComponent],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        ConfirmDialogModule
+      ],
+      providers: [
+        ConfirmationService,
+        MessageService,
+        { provide: AdminService, useValue: serviceAdmin },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -18,8 +38,31 @@ describe('AddProductComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  it('form invalid when empty', () => {
+    expect(component.forms.valid).toBeFalsy();
+});
+
+  it('should display my-image.png', () => {
+    const img = fixture.debugElement.nativeElement.querySelector('img');
+    const spyError = spyOn(component, 'uploadimg' ).and.callThrough();
+    img.dispatchEvent(new Event('change'));
+
+    fixture.detectChanges();
+
+    expect(spyError).toHaveBeenCalled();
+});
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const mockTableData = {
+      name: 'dddd',
+      img: 'C:\\fakepath\\1111.jpg',
+      price: 344,
+      vote: 444,
+      view: 444
+    };
+    const confirmationService: ConfirmationService = TestBed.get(ConfirmationService);
+    serviceAdmin.addProduct.and.returnValue(of({ status: 200 }));
+    component.onSubmit(mockTableData);
+    expect(confirmationService).toBeDefined();
   });
 });
--- src/app/layout/admin/add-product/add-product.component.ts
+++ src/app/layout/admin/add-product/add-product.component.ts
@@ -11,71 +11,80 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 })
 export class AddProductComponent implements OnInit {
   uploadedFiles: any[] = [];
-  forms: FormGroup
-  filename:string
-  selecttedFile:File
+  forms: FormGroup;
+  filename: string;
+  selecttedFile: File;
   httpOptions = {
     headers: new HttpHeaders({
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods':'GET,POST,OPTIONS,DELETE,PUT',
-    'Content-Type': 'application/JSON'
-  })
-  };
-  constructor(private fn: FormBuilder,private serviceAdmin:AdminService, private http:HttpClient,private confirm:ConfirmationService,private mess:MessageService) { }
-
-  ngOnInit(): void {
-    this.forms= this.fn.group({
-      name:['',[Validators.required]],
-      img:['',[Validators.required]],
-      price:['',[Validators.required]],
-      vote:['',[Validators.required]],
-      view:['',[Validators.required]]
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
+      'Content-Type': 'application/JSON'
     })
+  };
+  constructor(
+    private fn: FormBuilder,
+    private serviceAdmin: AdminService,
+    private http: HttpClient,
+    private confirm: ConfirmationService,
+    private mess: MessageService) { }
+  ngOnInit(): void {
+    this.forms = this.fn.group({
+      name: ['', [Validators.required]],
+      img: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      vote: ['', [Validators.required]],
+      view: ['', [Validators.required]]
+    });
   }
-  uploadimg(e:any){
-    this.selecttedFile = e.target.files[0]
-    const type = this.selecttedFile.type
-    if(!type.match(/image\/*/)){
-      this.mess.add({key: 'c',severity:'error', summary: 'Error', detail: 'AIncorrect format, please upload image file', life: 3000})
-      this.forms.controls.img.reset()
+  uploadimg(e: any) {
+    console.log(e);
+    this.selecttedFile = e.target.files[0];
+    const type = this.selecttedFile.type;
+    if (!type.match(/image\/*/)) {
+      this.mess.add({ key: 'c', severity: 'error', summary: 'Error', detail: 'AIncorrect format, please upload image file', life: 3000 });
+      this.forms.controls.img.reset();
     }
   }
-  onSubmit(e:any){
-    console.log(e);
+  onSubmit(e: any) {
+    console.log('data', e);
     this.confirm.confirm({
       message: 'Are you sure you want to Add new Product?',
       header: 'Confirm',
       icon: 'pi pi-exclamation-triangle',
       accept: () => {
-        e.img = this.selecttedFile.name
-        this.serviceAdmin.addProduct(e).subscribe();
-        const uploadData= new FormData();
-        uploadData.append("myfile", this.selecttedFile,this.selecttedFile.name)
-        this.http.post('http://localhost:80/angular/upload.php',uploadData,{
-          reportProgress:true,
-          observe:'events'
+        e.img = this.selecttedFile.name;
+        this.serviceAdmin.addProduct(e).subscribe({
+          next: c => {
+          },
+          error: error => {
+            this.mess.add({ key: 'c', severity: 'error', summary: 'Error', detail: 'Add Product Error', life: 3000 });
+          },
+          complete: () => {
+            this.mess.add({ key: 'c', severity: 'success', summary: 'Successful', detail: 'Add Product Complete', life: 3000 });
+          }
+        });
+        const uploadData = new FormData();
+        uploadData.append('myfile', this.selecttedFile, this.selecttedFile.name)
+        this.http.post('http://localhost:80/angular/upload.php', uploadData, {
+          reportProgress: true,
+          observe: 'events'
         }).subscribe(
-          data=>console.log(data)
-        )
-        this.mess.add({key: 'c',severity:'success', summary: 'Successful', detail: 'Add Product Complete', life: 3000});
-      //  setTimeout(() => {
-      //   this.serviceAdmin.getProduct().subscribe(data=>{
-      //     console.log(data);
-      //     data.forEach(el=>
-      //       this.http.post('http://localhost:2525/imposters/4545/stubs',el,this.httpOptions).subscribe(
-      //            data=>{
-      //              console.log(data)
-      //            },
-      //            err=>console.log(err)
-                 
-      //          )
-      //       )
-               
-      //        })
-      //  }, 2000);
-          
-    
-    }
-  })
+          data => console.log(data)
+        );
+        //  setTimeout(() => {
+        //   this.serviceAdmin.getProduct().subscribe(data=>{
+        //     console.log(data);
+        //     data.forEach(el=>
+        //       this.http.post('http://localhost:2525/imposters/4545/stubs',el,this.httpOptions).subscribe(
+        //            data=>{
+        //              console.log(data)
+        //            },
+        //            err=>console.log(err)
+        //          )
+        //       )
+        //        })
+        //  }, 2000);
+      }
+    });
   }
 }
